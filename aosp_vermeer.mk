c94b0d258f4392141e28144ee21e19bf4518ba51
DqrKnzx <DavidYonatan9@hotmail.com>
2024-08-24 20:33:46 -0600

    vermeer: Implement aosp_vermeer.mk

---
+++ aosp_vermeer.mk
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+
+# Inherit from the device configuration.
+$(call inherit-product, device/xiaomi/vermeer/device.mk)
+
+# Inherit from the AOSP configuration.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+
+PRODUCT_NAME := aosp_vermeer
+PRODUCT_DEVICE := vermeer
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_BRAND := POCO
+PRODUCT_MODEL := vermeer
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_PRODUCT=vermeer
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+# Boot Animation
+TARGET_BOOT_ANIMATION_RES := 1440
